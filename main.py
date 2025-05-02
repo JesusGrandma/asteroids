5102f65e357a58600ecf4f6045d097c395ba7059
JesusGrandma <bvilli53@gmail.com>
2025-05-02 11:11:53 -0400

    added player, asteroid field, basic asteroids, shots, various constants, update player to move

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
 import pygame
 import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroiodfield import AsteroidField
+from shot import Shot
 
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("My Game")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    
+    Player.containers = updatable, drawable
+    Asteroid.containers = asteroids, updatable, drawable
+    AsteroidField.containers = updatable
+    Shot.containers = shots, updatable, drawable
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        pygame.display.flip()
 
-    print(f"Starting Asteroids! Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("Game Over!")
+                pygame.quit
+                sys.exit()
+
+        for obj in drawable:
+            obj.draw(screen)
+
+
 
 
 
